1ac9c3df58df2b21be8e57adf098034515b5fc6a
Benjamin LeBrun <bl.lebrun@gmail.com>
2020-02-18 12:39:56 -0700

    makefiles, structured dir

--- Lab3/part1.c
+++ Lab3/part1.c
@@ -6,8 +6,8 @@ char buffer[4096];
 int i=0;
 int sum=0;
 
-int checksum(char buffer[]){
-    for (i=0;buffer[i] !=0; sum += buffer[i++]);
+int checksum(char buffer[]) {
+    for (i=0;buffer[i] != 0; sum += buffer[i++]);
     return sum;
 }
 
@@ -15,7 +15,7 @@ int main() {
     printf("Enter text (ctrl-D to end): ");
     do
         buffer[i]=getchar();
-    while((buffer[i]!=-1) && (++i < 4095));
+    while((buffer[i] != -1) && (++i < 4095));
 
     buffer[i]=0;
     printf("%s\n",buffer);
